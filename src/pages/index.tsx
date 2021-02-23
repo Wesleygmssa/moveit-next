c0add2b862806984b5679337bb7993a85b4119f5
wesley guerra <guerra_ssa@hotmail.com>
2021-02-23 12:11:04 -0300

    Migration das pasta para next

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import GlobalStyled, { Container } from "../styles/global";
+
+
+export default function Home() {
+  return (
+    <Container>
+      <GlobalStyled />
+      <ExperienceBar />
+    </Container>
+  )
+}
