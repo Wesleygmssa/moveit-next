5d0e14074fd1fc1be6888c192a5390746300c262
wesley guerra <guerra_ssa@hotmail.com>
2021-02-23 17:30:23 -0300

    create coutndown

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,11 @@
-import react from 'react';
+import react, { ButtonHTMLAttributes } from 'react';
 import { Container } from './styles';
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> //TODOS ELEMENTOS DE BUTTON EX: TYPE, VALUE E <ETC className="6"></ETC>
 
-const Button: React.FC = ({ children }) => {
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
 
     return (
-        <Container>
+        <Container {...rest}>
             {children}
         </Container>
     )
--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,24 +1,46 @@
-import react from 'react';
+import { ButtonHTMLAttributes, useEffect, useState } from 'react';
 import Button from '../Button';
 import { Container } from './styles';
 
 const Countdown: React.FC = () => {
+    const [time, setTime] = useState(25 * 60);
+    const [active, setActive] = useState(false);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondseLeft, secondsRight] = String(seconds).padStart(2, '0').split('');
+
+    function startCountdown() {
+        setActive(true);
+        console.log('teste')
+
+    }
+
+    useEffect(() => {
+        if (active && time > 0) {
+            setTimeout(() => {
+                setTime(time - 1);
+            }, 1000)
+        }
+    }, [active, time])
+
 
     return (
         <>
             <Container>
                 <div>
-                    <span>2</span>
-                    <span>5</span>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
                 </div>
                 <span>:</span>
                 <div>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{secondseLeft}</span>
+                    <span>{secondsRight}</span>
                 </div>
 
             </Container>
-            <Button>Iniciar um clico</Button>
+            <Button type="button" onClick={startCountdown} >Iniciar um ciclo </Button>
         </>
     )
 }
